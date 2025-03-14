9655f537d2dbfe0a604b0dd6c6225e46757b844d
Mikko Strahlendorff <mikko.strahlendorff@fmi.fi>
2025-03-14 11:02:28 +0000

    Added rh, ws and wg to predictands

--- bin/add-predictors-oceanids.py
+++ bin/add-predictors-oceanids.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import sys, json
-# Script to add predictors to the training dataset for OCEANIDS
-# (AK 2025)
+import numpy as np  # added for ERA5 predictors
 
 harbor_name = sys.argv[1]
 
@@ -16,6 +15,36 @@ df['dayOfYear'] = df['utctime'].dt.dayofyear
 df['year'] = df['utctime'].dt.year
 df['month'] = df['utctime'].dt.month
 df['hour'] = df['utctime'].dt.hour
+
+# --- ERA5 Predictors: Wind Speed, Wind Direction, and Relative Humidity ---
+for i in range(1, 5):
+    # Wind Speed and Direction per measurement index
+    u_00 = df[f'u10-00-{i}']
+    u_12 = df[f'u10-12-{i}']
+    v_00 = df[f'v10-00-{i}']
+    v_12 = df[f'v10-12-{i}']
+    
+    # Compute the average for u and v
+    u_avg = (u_00 + u_12) / 2
+    v_avg = (v_00 + v_12) / 2
+    
+    # Calculate wind speed and meteorological wind direction
+    df[f'ws-{i}'] = np.sqrt(u_avg**2 + v_avg**2)
+    df[f'wd-{i}'] = (270 - np.degrees(np.arctan2(v_avg, u_avg))) % 360
+
+    # Relative Humidity from 2m dewpoint and 2m temperature per measurement index
+    td2_00 = df[f'td2-00-{i}']
+    td2_12 = df[f'td2-12-{i}']
+    t2_00 = df[f't2-00-{i}']
+    t2_12 = df[f't2-12-{i}']
+    
+    # Compute average dewpoint and temperature
+    dew_avg = (td2_00 + td2_12) / 2
+    temp_avg = (t2_00 + t2_12) / 2
+    
+    # Calculate relative humidity using the standard formula
+    df[f'rh-{i}'] = 100 * (np.exp((17.625 * dew_avg) / (243.04 + dew_avg)) / 
+                           np.exp((17.625 * temp_avg) / (243.04 + temp_avg)))
 
 # add pressure change from previous day to predictors
 for i in range(1, 5):  # For columns 1 to 4
@@ -46,17 +75,13 @@ for predictand, mapping in predictand_mappings.items():
     df = df.merge(monthly_stats, on=['year', 'month'], how='left')
 
     # Climatology file for SF prediction
-    # Group by 'month' and calculate climatology (mean) for the specified columns
     monthly_climatology = df.groupby('month')[[f'{predictand}_mean', f'{predictand}_max', f'{predictand}_min']].mean().reset_index()
-    # Rename the columns
     monthly_climatology.rename(columns={
         f'{predictand}_mean': f'{predictand}_mean_climatology',
         f'{predictand}_max': f'{predictand}_max_climatology',
         f'{predictand}_min': f'{predictand}_min_climatology'
     }, inplace=True)
-    # Merge the climatology data back into the original DataFrame
     df = df.merge(monthly_climatology, on='month', how='left')
-    # Filter the DataFrame for the year 2020 to create a climatology csv for seasonal forecast prediction
     df_2020 = df[df['year'] == 2020][['utctime', 'month']]
     df_2020 = df_2020.merge(monthly_climatology, on='month', how='left')
     df_2020 = df_2020[['utctime', f'{predictand}_mean_climatology', f'{predictand}_max_climatology', f'{predictand}_min_climatology']]
